8f251ea38684707d223fadbddc9b2c9668d98e07
VictorZinko <zinkoexp@gmail.com>
2018-09-05 01:41:11 +0300

    Added C# HW1

---
+++ C#/HW/HW1/HomeWork1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            BTask();
+
+            Console.Clear();
+            CTask();
+
+            Console.Clear();
+            DTask();
+        }
+        
+        //b task
+        static void BTask()
+        {
+            int a;
+            try
+            {
+                Console.Write("Square length  = ");
+                a = Int32.Parse(Console.ReadLine());                
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erorr square length!");
+                a = 0;               
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Erorr! Value was either too large or too small!");
+                a = 0;                
+            }
+
+            Console.WriteLine("\nArea = {0}", a * a);
+            Console.WriteLine("Perimeter  = {0}", 4 * a);
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        //c task
+        static void CTask()
+        {
+            string name;
+            short age;
+
+            
+                Console.Write("What is your name? ");
+                name = Console.ReadLine();
+            try
+            {
+                Console.Write("How old are you? ");
+                age = Int16.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erorr age!");
+                age = 0;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Erorr! Value was either too large or too small!");
+                age = 0;
+            }
+
+            Console.WriteLine("\nHello {0}!", name);
+            Console.WriteLine("You are {0}!", age);            
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        //d task
+        static void DTask()
+        {
+            const double PI = 3.14;
+            double r;
+            
+            try
+            {
+                Console.Write("Give me a radius of a circle: ");
+                r = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erorr age!");
+                r = 0;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Erorr! Value was either too large or too small!");
+                r = 0;
+            }
+
+            Console.WriteLine("\nLength = {0}", 2 * PI * r);
+            Console.WriteLine("Area = {0}", PI * r * r);
+            Console.WriteLine("Volume = {0}", ((4 / 3) * PI * (r * r * r)));
+
+            Console.Write("\nPress any key to exit . . . ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/HW/HW1/Task1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            FirstTask();
+
+            Console.Clear();
+            SecondTask();
+
+            Console.Clear();
+            ThirdTask();
+
+            Console.Clear();
+            FourthTask();
+
+        }
+
+        //First task
+        static void FirstTask()
+        {
+            int a, b;
+
+            try
+            {
+                Console.Write("a = ");
+                a = Int32.Parse(Console.ReadLine());
+                Console.Write("b = ");
+                b = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erorr in value!");
+                a = 0;
+                b = 0;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Erorr! Value was either too large or too small!");
+                a = 0;
+                b = 0;
+            }
+            
+
+            Console.WriteLine("\na+b = {0}", a + b);
+            Console.WriteLine("a-b = {0}", a - b);
+            Console.WriteLine("a*b = {0}", a * b);
+
+            if ( a != 0 && b != 0 )
+            {
+                Console.WriteLine("a/b = {0}", a / b);
+            }
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        //Second task
+        static void SecondTask()
+        {
+            string answer;
+            Console.Write("How are you?:");
+            answer = Console.ReadLine();
+            Console.WriteLine("\nYou are {0}", answer);
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        //Third task
+        static void ThirdTask()
+        {
+            char firstChar, secondChar, thirdChar;
+            try
+            {
+                Console.Write("Give me a first char: ");
+                firstChar = char.Parse(Console.ReadLine());
+                Console.Write("Give me a second char: ");
+                secondChar = char.Parse(Console.ReadLine());
+                Console.Write("Give me 3 char: ");
+                thirdChar = char.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect char!");
+                firstChar = ' ';
+                secondChar = ' ';
+                thirdChar = ' ';
+            }
+
+            Console.WriteLine("You enter ({0}), ({1}), ({2})", firstChar,secondChar,thirdChar);
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        //Fourth task
+        static void FourthTask()
+        {
+            int [] numbers = { 0, 0 };
+            bool checkFirstNumber, checkSecondNumber;
+            Console.Write("Give me a first number: ");
+            checkFirstNumber = Int32.TryParse(Console.ReadLine(), out numbers[0]);
+            Console.Write("Give me a second number: ");
+            checkSecondNumber = Int32.TryParse(Console.ReadLine(), out numbers[1]);
+
+            Console.WriteLine("\nFirst number: {0}\nSecond number: {1}", checkFirstNumber, checkSecondNumber);
+
+            Console.Write("\nPress any key to exit . . . ");
+            Console.ReadKey();
+        }
+    }
+       
+}
