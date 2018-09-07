8a4071ed3d5d7e7dbb33ab8aa97c36d1be453578
VictorZinko <zinkoexp@gmail.com>
2018-09-07 13:57:25 +0300

    Task4 v1.0

---
+++ CSharp/HW/HW4/Task4/Task4/Car.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Car
+    {
+        public const string CompanyName = "Ford Motor Company";
+        private string name;
+        private string color;
+        private double price;
+        
+        public string Color {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public Car (string name = "none", string color = "none", double price=0.0)
+        {
+            this.name = name;
+            this.color = color;
+            this.price = price;
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("\nADD A NEW CAR");
+            Console.Write("Enter name:");
+            name = Console.ReadLine();
+
+            Console.Write("Enter color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter price:");
+            try
+            {
+                price = double.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERORR!");
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("\nINFORMATION ABOUT CAR");
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Color: {0}", color);
+            Console.WriteLine("Price: {0}", price);
+        }
+
+        public void ChangePrice(double x)
+        {
+            price = price - (price * (x / 100));
+        }
+        public override string ToString()
+        {
+            return ("Car " + name + " " + "Color " + color + " " + "Price " + price);
+        }
+        public static bool operator ==(Car first, Car second)
+        {
+            return first.name.Equals(second.name) && Math.Round(first.price, 2).Equals(Math.Round(second.price, 2));
+        }
+        public static bool operator !=(Car first, Car second)
+        {
+            return !first.name.Equals(second.name) && Math.Round(first.price, 2).Equals(Math.Round(second.price, 2));
+
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            Car obj1 = (Car)obj;
+            return name.Equals(obj1.name) && price.Equals(obj1.price);
+        }
+        public override int GetHashCode()
+        {
+            return name.GetHashCode() ^ price.GetHashCode() ^ color.GetHashCode();
+        }
+    }
+}
---
+++ CSharp/HW/HW4/Task4/Task4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Car.CompanyName);
+            Car car1 = new Car();
+            Car car2 = new Car("mondeo", "white", 13000);
+            Car car3 = new Car();
+            Car car4 = new Car("mondeo", "black", 13000);
+
+            //car1.Input();
+            //car3.Input();
+               
+            car1.ChangePrice(10);
+            car2.ChangePrice(10);
+            car3.ChangePrice(10);
+            car4.ChangePrice(10);
+
+            car1.Print();
+            car2.Print();
+            car4.Print();
+            car3.Print();
+
+            car2.Color = "Blue";
+
+            car2.Print();
+            car4.Print();
+
+            Console.WriteLine("\ncar2 == car4: {0}", car2==car4);
+            Console.WriteLine("\ncar2.Equals(car4): {0}", car2.Equals(car4));
+
+            Console.WriteLine("\ncar2.ToString(): {0}", car2.ToString());
+
+            Console.Write("\nPress any key to exit . . . ");
+            Console.ReadKey();
+
+        }
+    }
+}
