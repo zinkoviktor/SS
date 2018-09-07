5cb6a2a0628e95bd5de2afbe49ebc37e0737f460
VictorZinko <zinkoexp@gmail.com>
2018-09-07 12:32:15 +0300

    HW5.2 v1.0

---
+++ CSharp/HW/HW5/HW5.2/HW5.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<uint, string> persons = new Dictionary<uint, string>();
+
+            persons.Add(101, "Alex");
+            persons.Add(102, "Bernard");
+            persons.Add(103, "Clark");
+            persons.Add(105, "Ellis");
+            persons.Add(106, "Grace");
+            persons.Add(109, "Kevin");
+            persons.Add(110, "Monica");
+
+            uint ID = 0;
+
+            Console.Write("Enter ID:");
+            try
+            {
+                ID = uint.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR!");
+            }
+
+            if (persons.ContainsKey(ID))
+            {
+                Console.WriteLine("Name = {0}", persons[ID]);
+            }
+            else
+            {
+                Console.WriteLine("Nothing by this ID");
+            }
+
+            Console.Write("\nPress any key to exit . . . ");
+            Console.ReadKey();
+        }
+    }
+}
