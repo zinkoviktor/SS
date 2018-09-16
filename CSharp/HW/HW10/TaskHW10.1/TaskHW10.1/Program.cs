b7d0bb4f4fa0163c3b3ba2a2b55e17c8b57de858
VictorZinko <zinkoexp@gmail.com>
2018-09-16 23:46:11 +0300

    HW10.1 v1.0

---
+++ CSharp/HW/HW10/TaskHW10.1/TaskHW10.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskHW10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RealNumberDelegate del1 = new RealNumberDelegate(RealNumber);
+            RealNumberDelegate del2 = new RealNumberDelegate(Sin);
+            Tabulation(del1.Invoke(4.4000000), 3, 5, 2);
+            Tabulation(del2(4 / 5), 4, 5, 6);
+            Console.ReadKey();
+        }
+        public delegate double RealNumberDelegate(double d);
+        
+        public static double RealNumber(double d)
+        {
+            return d;
+        }
+        
+        public static double Sin(double x)
+        {
+            return 2 * x / 2 + 3 * x * Math.Cos(x / 3);
+        }
+        
+        public static void Tabulation(double k, double a, double b, double n)
+        {
+            Console.WriteLine((a + k * (b - a) / n));
+        }
+        
+    }
+}
