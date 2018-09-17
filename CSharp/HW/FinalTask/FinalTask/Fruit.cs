c0c83cd80efb9cfd2fe83e4ac7b3ea01412bd48d
VictorZinko <zinkoexp@gmail.com>
2018-09-18 02:33:10 +0300

    FinalTask v2.0

--- CSharp/HW/FinalTask/FinalTask/Fruit.cs
+++ CSharp/HW/FinalTask/FinalTask/Fruit.cs
@@ -9,16 +9,48 @@ namespace FinalTask
 {
     class Fruit
     {
-        public string name { get; set; }
-        public string color { get; set; }
+        //Colors in HEX
+        enum Colors 
+        {           
+            Black = 0x0,
+            Red = 0xFF,
+            Green = 0xFF00,
+            Yellow = 0xFFFF,
+            Blue = 0xFF0000,
+            Magenda = 0xFF00FF,
+            Cyan = 0xFFFF00,
+            White = 0xFFFFFF,
+            None = -1
+        }
 
+        private string name; //Fruit name, default null
+        private Colors color = Colors.None; //Fruit color in HEX from Enum Colors
+        
+        public string Name
+        {
+            get
+            {
+                return name;
+            }                
+        }
+        public string Color
+        {
+            get
+            {
+                return color.ToString();
+            }
+        }
+
+        //Constructors
         public Fruit() { }
         public Fruit(string name, string color)
         {
             this.name = name;
-            this.color = color;
+            this.color = parseToColorsKey(color); //Parses string color to enum keyword           
         }
 
+        ///<summary>Inputs name and color from console</summary>
+        //If fruit is created, writes message
         virtual public void Input()
         {
             if (name == null)
@@ -26,51 +58,104 @@ namespace FinalTask
                 Console.Write("Enter name: ");
                 name = Console.ReadLine();
             }
-            if (color == null)
+            else if (color.Equals(Colors.None))
             {
-                Console.Write("Enter name: ");
-                color = Console.ReadLine();
+                Console.WriteLine("Name = {0}", Name);
+                Console.Write("Enter color: ");
+                color = parseToColorsKey(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Cannot change created Fruit!");
+                Print();
             }
         }
+
+        /// <summary>Prints name and color into console</summary>
+        //If fruit is empty, writes message
         virtual public void Print()
         {
             if (name != null)
             {
-                Console.WriteLine("Fruit name = {0}", name);
-
-                if (color != null)
-                {
-                    Console.WriteLine(", color = {0}", color);
-                }
-                else
-                {
-                    Console.WriteLine(", color = none");
-                }
+                Console.WriteLine("Fruit name = {0}, color = {1}", Name, Color);
             }            
             else
             {
                 Console.WriteLine("Empty fruit");
             }
         }
-        virtual public void Input(string path)
+
+        /// <summary>Inputs name and color from file</summary>
+        /// <param name="sr">Used to reads from file</param>
+        virtual public void Input(StreamReader sr)
         {
-            using (StreamReader reader = new StreamReader(path))
+            string line;
+            string[] values;
+
+            if (sr.Peek() > 0)
             {
-                    if (reader.Peek() > 0)
-                    {
-                        name = reader.ReadLine();
-                        color = reader.ReadLine();
-                    }
-            }      
+                line = sr.ReadLine();
+                values = line.Split('/');
+                if (lineValidator(values))
+                {
+                    name = values[1];
+                    color = parseToColorsKey(values[2]);
+                }
+            }                      
         }
+
+        /// <summary>Writes name and color into file</summary>
+        /// <param name="path">Used to get file path</param>
         virtual public void Print(string path)
         {
             using (StreamWriter writer = new StreamWriter(path, true))
             {
-                writer.WriteLine(name);
-                writer.WriteLine(color);
+                writer.WriteLine("1/" + Name + "/" + Color);                
             }
         }
+
+        //Changes first string character to UpperCase
+        private string firstCharToUpper(string str)
+        {
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        //Parses string color name to the enum keyword
+        private Colors parseToColorsKey(string color)
+        {
+            try
+            {
+                return (Colors)Enum.Parse(typeof(Colors), firstCharToUpper(color));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return Colors.None;
+            }
+        }
+
+        /// <summary>???</summary>
+        /// <param name="str">???</param>        
+        public bool lineValidator(string[] str)
+        {
+            try
+            {
+                int key = int.Parse(str[0]);
+                string name = str[1];
+                string color = str[2];
+                if (key == 2)
+                {
+                    double vitCLvl = double.Parse(str[3]);
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
         public override string ToString()
         {
             return name;
--- CSharp/HW/FinalTask/FinalTask/Program.cs
+++ CSharp/HW/FinalTask/FinalTask/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,64 @@ namespace FinalTask
 {
     class Program
     {
+        public enum FruitsId
+        {
+            Fruit = '1',
+            Citrus
+        }
         static void Main(string[] args)
         {
+            List<Fruit> fruits = new List<Fruit>(); //List for some fruits
+            string readPath = @"file.txt"; //path to file, with data of fruits        
+
+            //Adds fruits from file
+            using (StreamReader sr = new StreamReader(readPath))
+            {
+                int count = 3;
+                int peekLine;
+                {
+                    do
+                    {
+                        peekLine = sr.Peek();
+
+                        if (peekLine == (char)FruitsId.Fruit)
+                        {
+                            try
+                            {
+                                fruits.Add(new Fruit());
+                                fruits[fruits.Count - 1].Input(sr);
+                                count--;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                        }
+                        else if (peekLine == (char)FruitsId.Citrus)
+                        {
+                            Console.WriteLine("{0} - {1}", sr.Peek(), sr.ReadLine());                            
+                            count--;
+                        }
+                        else if(peekLine > 0)
+                        {
+                            sr.ReadLine();                            
+                        }                        
+                        else
+                        {
+                            count = 0;
+                        }
+                    }
+                    while (count > 0);
+                }                
+            }
+
+            foreach(Fruit fruit in fruits)
+            {
+                fruit.Print();
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
