f3ddd2b6b7003b9664bf913a7ba5309de306eded
VictorZinko <zinkoexp@gmail.com>
2018-09-18 10:45:58 +0300

    TaskHW10.1 v2.0

--- CSharp/HW/HW10/TaskHW10.1/TaskHW10.1/Program.cs
+++ CSharp/HW/HW10/TaskHW10.1/TaskHW10.1/Program.cs
@@ -10,27 +10,33 @@ namespace TaskHW10._1
     {
         static void Main(string[] args)
         {
-            RealNumberDelegate del1 = new RealNumberDelegate(RealNumber);
+            RealNumberDelegate del1 = new RealNumberDelegate(FuncGraph);
             RealNumberDelegate del2 = new RealNumberDelegate(Sin);
-            Tabulation(del1.Invoke(4.4000000), 3, 5, 2);
-            Tabulation(del2(4 / 5), 4, 5, 6);
+            Tabulation(3, 5, 3, del1);
+            Tabulation(4, 5, 2, del2);
             Console.ReadKey();
         }
         public delegate double RealNumberDelegate(double d);
         
-        public static double RealNumber(double d)
-        {
-            return d;
-        }
-        
-        public static double Sin(double x)
+        public static double FuncGraph(double x)
         {
             return 2 * x / 2 + 3 * x * Math.Cos(x / 3);
         }
         
-        public static void Tabulation(double k, double a, double b, double n)
+        public static double Sin(double x)
         {
-            Console.WriteLine((a + k * (b - a) / n));
+            return Math.Sin(x);
+        }
+        
+        public static void Tabulation(int a, int b, int n, RealNumberDelegate del)
+        {
+            int k = 0;
+            double x = (a + k * (b - a) / n);
+            for (; k < n; k++)
+            {
+                Console.WriteLine(del(x));
+            }
+            
         }
         
     }
